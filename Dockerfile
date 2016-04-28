0d78fdb9ae4f558e06a180492cee8bc9006b73cd
Martin <martin@effectivetrainings.de>
2016-04-28 22:02:58 +0200

    initial

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM oracle/openjdk:8
+ADD target/demo-0.0.1-SNAPSHOT.jar /
+EXPOSE 8080
+CMD java -jar /demo-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class DemoApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 2000L)
+	public void schedule() {
+		LOGGER.info("This is an info");
+		LOGGER.warn("This is a warn");
+
+	}
+}
